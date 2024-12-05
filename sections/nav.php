22dca17aba44ea02439a3aafa1065d6df1c07fc7
cosmas-book <info@cosmas.org>
2024-12-05 17:30:14 +0530

    svsd

--- sections/nav.php
+++ sections/nav.php
@@ -1,29 +1,25 @@
-<!-- Navigaion -->
-<!-- <nav  class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+<!-- Fixed Navigation Bar -->
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
     <div class="container">
-        <ul class="nav  justify-content-center">
-            <li class="nav-item"><a  href="#home">Home</a></li>
-            <li class="nav-item"><a  href="#about">About Me</a></li>
-            <li class="nav-item"><a  href="#experience">Experience</a></li>
-            <li class="nav-item"><a  href="#education">Education</a></li>
-            <li class="nav-item"><a  href="#skills">Skills</a></li>
-            <li class="nav-item"><a  href="#research">Research</a></li>
-            <li class="nav-item"><a  href="#contact">Contact</a></li>
-        </ul>
+        <a class="navbar-brand" href="#">Your Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
-</nav> -->
-
-<section class="container">
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" data-bs-toggle="tab" href="#home">Home</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-bs-toggle="tab" href="#menu1">Menu 1</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-bs-toggle="tab" href="#menu2">Menu 2</a>
-  </li>
-</ul>
-</section>
-
+</nav>
