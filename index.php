c054179aa3d5b1bb93c640737deb965673c46b20
cosmas-book <info@cosmas.org>
2024-12-05 17:32:53 +0530

    sasasd

--- index.php
+++ index.php
@@ -23,8 +23,7 @@
     <link rel="shortcut icon" href="./public/assets/images/DSC_0032.ico" type="image/x-icon">
     
     <!-- font awesome files -->
-    <link rel="stylesheet" href="./public/assets/fontawesome/css/all.css">
-    <link rel="stylesheet" href="./public/assets/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     
 
     <!-- bootstrap CDN -->
--- sections/nav.php
+++ sections/nav.php
@@ -1,5 +1,5 @@
 <!-- Fixed Navigation Bar -->
-<nav id="navbar" class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light  bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">Your Logo</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,3 +23,4 @@
         </div>
     </div>
 </nav>
+<!-- fixed-top -->
